0a169083846ef1549286ab9b49e46077af3958c2
mateja <ma.petrovic@levi9.com>
2019-07-30 13:56:46 +0200

    feat(bit): exports Hello component

---
+++ dist/src/components/Hello/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./Hello"));
+var Hello_1 = require("./Hello");
+exports.default = Hello_1.default;
+//# sourceMappingURL=index.js.map
+
+//# sourceMappingURL={"version":3,"file":"index.js","sourceRoot":"","sources":["index.ts"],"names":[],"mappings":";;;;;AAAA,6BAAwB;AACxB,iCAAkC;AAAzB,0BAAA,OAAO,CAAA"}
